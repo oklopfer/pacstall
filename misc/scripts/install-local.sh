oklopfer/pacstall

commit 51e085606fc76c9230034d2de1a14da37b4fbde2

Fixed more stuff

misc/scripts/install-local.sh
@@ -104,9 +104,9 @@ if [[ $REMOVE_DEPENDS = y ]] ; then
 fi
 sudo rm -rf /tmp/pacstall/*
 cd $HOME
-echo "version=\"$version\"" | sudo tee /var/log/pacstall_installed/$PACKAGE >/dev/null
-echo "description=\"$description\"" | sudo tee -a /var/log/pacstall_installed/$PACKAGE >/dev/null
-echo "date=\"$(date)\"" | sudo tee -a /var/log/pacstall_installed/$PACKAGE >/dev/null
+echo "version=\"$version"\" | sudo tee /var/log/pacstall_installed/$PACKAGE >/dev/null
+echo "description=\"$description"\" | sudo tee -a /var/log/pacstall_installed/$PACKAGE >/dev/null
+echo "date=\"$(date)"\" | sudo tee -a /var/log/pacstall_installed/$PACKAGE >/dev/null
 fancy_message info "Symlinking files"
 cd /usr/src/pacstall/
 # By default (I think), stow symlinks to the directory behind it (..), but we want to symlink to /, or in other words, symlink files from pkg/usr to /usr

commit 55d5db774d499d324ae6a58f7dedda68681a884b

escape quotes correctly, broken since 51e0856 (may 2021)

misc/scripts/install-local.sh
@@ -65,29 +65,29 @@ function trap_ctrlc() {
 }
 
 function write_meta() {
-    echo "_name=\"$name"\"
-    echo "_version=\"${full_version}"\"
-    echo "_install_size=\"${install_size}"\"
+    echo "_name=\"$name\""
+    echo "_version=\"${full_version}\""
+    echo "_install_size=\"${install_size}\""
     printf '_date=\"%(%a %b %_d %r %Z %Y)T\"\n'
     if [[ -n $maintainer ]]; then
-        echo "_maintainer=\"${maintainer}"\"
+        echo "_maintainer=\"${maintainer}\""
     fi
     if [[ -n $ppa ]]; then
         echo "_ppa=(${ppa[*]})"
     fi
     if [[ -n $homepage ]]; then
-        echo "_homepage=\"${homepage}"\"
+        echo "_homepage=\"${homepage}\""
     fi
     if [[ -n $gives ]]; then
-        echo "_gives=\"$gives"\"
+        echo "_gives=\"$gives\""
     fi
     if [[ -f /tmp/pacstall-pacdeps-"$name" ]]; then
         echo '_pacstall_depends="true"'
     fi
     if [[ $local == 'no' ]]; then
-        echo "_remoterepo=\"$pURL"\"
+        echo "_remoterepo=\"$pURL\""
         if [[ $branch == 'yes' ]]; then
-            echo "_remotebranch=\"$pBRANCH"\"
+            echo "_remotebranch=\"$pBRANCH\""
         fi
     fi
     if [[ -n ${pacdeps[*]} ]]; then
@@ -102,7 +102,6 @@ function write_meta() {
     fi
 }
 
-# Logging metadata
 function meta_log() {
     # Origin repo info parsing
     if [[ ${local} == "no" ]]; then
@@ -183,7 +182,7 @@ function calc_git_pkgver() {
     comp_git_pkgver="${calc_commit:0:8}"
 }
 
-function compare_remote_version() (
+function compare_remote_version() {
     local input="${1}"
     unset -f pkgver 2> /dev/null
     source "$METADIR/$input" || return 1
@@ -218,7 +217,7 @@ function compare_remote_version() (
     else
         echo "no"
     fi
-)
+}
 
 function set_distro() {
     local distro_name="$(lsb_release -si 2> /dev/null)"